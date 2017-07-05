88d93c303f9e536e62f2ff16fa8f17a35606b484
wangxiaozhong <xiaozhong.wang@fengjr.com>
2017-07-05 13:36:41 +0800

    #issue搭建blog项目

--- wangxz-blog-admin/src/main/java/com/wangxz/controller/App.java
+++ wangxz-blog-admin/src/main/java/com/wangxz/controller/App.java
@@ -2,12 +2,16 @@ package com.wangxz.controller;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 1;
+        Integer c = new Integer(1);
+        Integer e = 129;
+        Integer f = 129;
+        System.out.print(a == b);
+        System.out.print(a == c);
+        System.out.print(e == f);
     }
 }
