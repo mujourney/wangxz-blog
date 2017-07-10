cdb3fb962cc1444269a0d2edd0e377cc55d90ed2
wangxiaozhong <xiaozhong.wang@fengjr.com>
2017-07-10 18:33:59 +0800

    #issue修改模块

--- wangxz-blog-web/src/main/java/com/wangxz/controller/App.java
+++ wangxz-blog-web/src/main/java/com/wangxz/controller/App.java
@@ -2,12 +2,24 @@ package com.wangxz.controller;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int test = test();
+        System.out.println(test);
+    }
+
+    public static int test() {
+        int i = 0;
+        try {
+            i++;
+            new Exception();
+            return 10;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 100;
+        } finally {
+            return 1000;
+        }
     }
 }
