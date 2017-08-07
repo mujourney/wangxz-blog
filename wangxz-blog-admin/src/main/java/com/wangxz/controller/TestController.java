b4c34a98cd6d0cf15f8480bc2cb11413bb791007
Zhou <zz.mark06@gmail.com>
2017-08-07 09:23:27 +0800

    admin模块添加相关依赖和相关配置

--- wangxz-blog-admin/src/main/java/com/wangxz/controller/TestController.java
+++ wangxz-blog-admin/src/main/java/com/wangxz/controller/TestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RefreshScope
+
 public class TestController {
     @Autowired
     TestService testService;
@@ -24,6 +25,4 @@ public class TestController {
         testService.test();
         return "aaa";
     }
-
-
 }
